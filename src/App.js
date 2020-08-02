2dca495b08ee5b2048d009ad10ea932879b2234b
Jerrycaffe <jerrycaffe@gmail.com>
2020-08-02 14:59:06 +0100

    added user edit profile for authenticated user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Axios from "axios";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "../src/styles/styles.css";
 import "./App.css";
+import AuthState from "./Context/authState";
 
-import CustomerResultPage from './components/SearchResultPage/resultPage'
+import CustomerResultPage from './components/SearchResultPage/resultPage';
 import Home from "./components/landingPage/Home";
 import ResultPage from "./components/SearchResultPage/resultPage";
 import AboutUs from "./components/landingPage/About";
@@ -11,13 +13,14 @@ import NewsLetter from "./components/landingPage/NewsLetter";
 import Login from "./components/login/Login";
 import SignUp from "./components/SignUp";
 import Footer from "./components/landingPage/Footer";
-import AuthState from "./Context/authState";
-import Axios from "axios";
-import Test from "./components/landingPage/Home";
-import resultPage from "./components/SearchResultPage/resultPage";
 
+import resultPage from "./components/SearchResultPage/resultPage";
 import AllCenters from "./components/allCenters/AllCenters";
+
+import UserProfile from "./components/dashboard/profile/EditProile"
+
 //save token in local storage, this will set the header authorization = localStorage.token
+
 if (localStorage.token) {
   Axios.defaults.headers.common["authorization"] = localStorage.token;
 }
@@ -34,6 +37,7 @@ function App() {
             component={CustomerResultPage}
           />
           <Route path="/all/centers" component={AllCenters} />
+          <Route path="/dashboard/profile/edit" component={UserProfile} />
           <Route path="/" exact component={Home} />
 
           <Redirect to="/" />
